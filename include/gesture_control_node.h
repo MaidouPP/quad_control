17afa570ea02843cab2bc16ab3fbf5da30a0e4a0
MaidouPP <leept416@gmail.com>
2019-04-23 17:02:38 -0700

    Fixes orientation by using setpoint_raw; still have problem with landing and speed change

--- include/gesture_control_node.h
+++ include/gesture_control_node.h
@@ -4,6 +4,8 @@
 #include <ros/ros.h>
 #include <geometry_msgs/PoseStamped.h>
 #include <mavros_msgs/CommandBool.h>
+#include <mavros_msgs/CommandTOL.h>
+#include <mavros_msgs/PositionTarget.h>
 #include <mavros_msgs/SetMode.h>
 #include <mavros_msgs/State.h>
 
@@ -14,21 +16,30 @@ class GestureControl{
  private:
   mavros_msgs::State _current_state;
   mavros_msgs::CommandBool _arm_cmd;
-  geometry_msgs::PoseStamped _pose;
+  // geometry_msgs::PoseStamped _pose;
+  mavros_msgs::PositionTarget _pose;
   ros::NodeHandle _nh;
   ros::Subscriber _state_sub = _nh.subscribe<mavros_msgs::State>
       ("mavros/state", 10, &GestureControl::StateCB, this);
   ros::Subscriber _gesture_sub = _nh.subscribe<gesture_recognition_client::Gesture>
       ("gesture_recognition/gesture", 10, &GestureControl::GestureCB, this);
-  ros::Publisher _local_pos_pub = _nh.advertise<geometry_msgs::PoseStamped>
-      ("mavros/setpoint_position/local", 10);
+  ros::Subscriber _pose_sub = _nh.subscribe<geometry_msgs::PoseStamped>
+      ("mavros/local_position/pose", 10, &GestureControl::CurrPoseCB, this);
+  // ros::Publisher _local_pos_pub = _nh.advertise<geometry_msgs::PoseStamped>
+  //     ("mavros/setpoint_position/local", 10);
+  ros::Publisher _local_pos_pub = _nh.advertise<mavros_msgs::PositionTarget>
+      ("mavros/setpoint_raw/local", 10);
   ros::ServiceClient _arming_client = _nh.serviceClient<mavros_msgs::CommandBool>
       ("mavros/cmd/arming");
+  ros::ServiceClient _land_client = _nh.serviceClient<mavros_msgs::CommandTOL>
+      ("mavros/cmd/land");
   ros::ServiceClient _set_mode_client = _nh.serviceClient<mavros_msgs::SetMode>
       ("mavros/set_mode");
   ros::Rate _rate;
   ros::Time _last_request = ros::Time::now();
   mavros_msgs::SetMode _offb_set_mode;
+  geometry_msgs::PoseStamped _current_pose;
+  float _lin_vel;
 
  public:
   GestureControl() : _rate(20.0), _pose() {};
@@ -37,7 +48,10 @@ class GestureControl{
   void StateCB(const mavros_msgs::State::ConstPtr& msg);
   // gesture_sub callback function
   void GestureCB(const gesture_recognition_client::Gesture::ConstPtr& msg);
+  // current pose callback function
+  void CurrPoseCB(const geometry_msgs::PoseStamped::ConstPtr& msg);
   void Run();
+  static double GetYaw(geometry_msgs::Quaternion q);
 };
 
 
--- src/gesture_control_node.cpp
+++ src/gesture_control_node.cpp
@@ -1,9 +1,19 @@
 #include <ros/ros.h>
 // #include <quad_control/gesture_control_node.h>
 #include "../include/gesture_control_node.h"
-#include <tf2/transform_datatypes.h>
 #include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+#include <tf2/LinearMath/Quaternion.h>
+#include <tf2/transform_datatypes.h>
+#include <cmath>
 #include <string>
+
+double GestureControl::GetYaw(geometry_msgs::Quaternion q) {
+  double roll, pitch, yaw;
+  tf2::Quaternion quat(q.x, q.y, q.z, q.w);
+  tf2::Matrix3x3 mat(quat);
+  mat.getRPY(roll, pitch, yaw);
+  return yaw;
+}
 
 void GestureControl::Initialize() {
   // wait for FCU connection
@@ -19,9 +29,11 @@ void GestureControl::Initialize() {
   _arm_cmd.request.value = true;
   ROS_INFO("Armed.");
 
-  _pose.pose.position.x = 0;
-  _pose.pose.position.y = 0;
-  _pose.pose.position.z = 2;
+  _pose.position.x = 0;
+  _pose.position.y = 0;
+  _pose.position.z = 2;
+
+  _lin_vel = 0.5;
 
   ROS_INFO("About to send setpoints.");
   for(int i = 100; ros::ok() && i > 0; --i){
@@ -38,22 +50,41 @@ void GestureControl::StateCB(const mavros_msgs::State::ConstPtr& msg) {
 }
 
 
+void GestureControl::CurrPoseCB(const geometry_msgs::PoseStamped::ConstPtr& msg) {
+  _current_pose = *msg;
+}
+
+
 void GestureControl::GestureCB(const gesture_recognition_client::Gesture::ConstPtr& msg) {
-  if (msg->gesture_name == "left_up") {
-    _pose.pose.position.z += 2;
-  } else if (msg->gesture_name == "go_left" || msg->gesture_name == "go_right") {
-    tf2::Quaternion q_orig, q_rot, q_new;
-    double r = 0, p = 0, y = 0;
-    tf2::convert(_pose.pose.orientation, q_orig);
+  if (msg->gesture_name == "left_up") {  // Go Up
+    _pose.position.z = _current_pose.pose.position.z + 2;
+  } else if (msg->gesture_name == "go_left" || msg->gesture_name == "go_right") {  // Turn left or turn right
+    double yaw = GetYaw(_current_pose.pose.orientation);
     if (msg->gesture_name == "go_right") {
-      y = 1.0;  // yaw by around 30 degree
+      _pose.yaw = yaw + 1.0;
     } else {
-      y = -1.0;
+      _pose.yaw = yaw - 1.0;
     }
-    q_rot.setRPY(r, p, y);
-    q_new = q_orig * q_rot;  // Calculate the new orientation
-    q_new.normalize();
-    tf2::convert(q_new, _pose.pose.orientation);
+  } else if (msg->gesture_name == "left_up_right_up") {  // Move forward until calling stop
+    double yaw = GetYaw(_current_pose.pose.orientation);
+    _pose.position.x = _current_pose.pose.position.x + cos(yaw) * 10;
+    _pose.position.y = _current_pose.pose.position.y + sin(yaw) * 10;
+  } else if (msg->gesture_name == "equal") {  // Hover in the air
+    _pose.position.x = _current_pose.pose.position.x;
+    _pose.position.y = _current_pose.pose.position.y;
+    _pose.position.z = _current_pose.pose.position.z;
+  } else if (msg->gesture_name == "cross") {  // Land to the origin
+    mavros_msgs::CommandTOL srv_land;
+    srv_land.request.altitude = 0;
+    srv_land.request.latitude = 0;
+    srv_land.request.longitude = 0;
+    srv_land.request.min_pitch = 0;
+    srv_land.request.yaw = 0;
+    if (_land_client.call(srv_land)) {
+      ROS_INFO("srv_land send ok %d", srv_land.response.success);
+    } else {
+      ROS_ERROR("Failed Land");
+    }
   } else {
     ROS_INFO("Cannot define this action for now.");
   }
@@ -70,9 +101,9 @@ void GestureControl::Run() {
       }
       _last_request = ros::Time::now();
     } else {
-      if(!_current_state.armed &&
-         (ros::Time::now() - _last_request > ros::Duration(5.0))){
-        if(_arming_client.call(_arm_cmd) &&
+      if (!_current_state.armed &&
+         (ros::Time::now() - _last_request > ros::Duration(5.0))) {
+        if (_arming_client.call(_arm_cmd) &&
            _arm_cmd.response.success){
           ROS_INFO("Vehicle armed");
         }
@@ -82,7 +113,7 @@ void GestureControl::Run() {
 
     _local_pos_pub.publish(_pose);
     std::string mode_str = _current_state.mode;
-    ROS_INFO(mode_str.c_str());
+    // ROS_INFO(mode_str.c_str());
 
     ros::spinOnce();
     _rate.sleep();
